c77c6a3994577fcc782ce41b2d88d2bcd25ce266
E Santhosh Kumar <esanthoshk09@gmail.com>
2026-03-02 11:36:31 +0000

    Add multiplication matrix C program

---
+++ MULTI.C
@@ -0,0 +1,12 @@
+
+#include<stdio.h>
+int main()
+{
+    int a,b,c;
+    printf("Enter the two numbers");
+    scanf("%d%d",&a,&b);
+    c=a*b;
+    printf("the multiplicatio of number is:%d",c);
+    return 0;
+
+}
