4a9b3eb465fb0ef05cb6e3e7a40577195ef30d8b
E Santhosh Kumar <esanthoshk09@gmail.com>
2026-03-03 06:35:33 +0000

    Add Addition of two numbers.c

---
+++ ADD.C
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int a,b,c;
+    printf("Enter the two numbers:");
+    scanf("%d%d",&a,&b);
+    c=a+b;
+    printf("Addition of two numbers: %d",c);
+    return 0;
+    getch();
+}
